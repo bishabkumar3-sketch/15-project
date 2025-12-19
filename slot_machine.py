52f74a9b13ee2f9703ab6427def90a6270a17a58
bishabkumar <bishabkumar3@.gmail>
2025-12-19 22:19:28 +0530

    this is slot machine game

---
+++ slot_machine.py
@@ -0,0 +1,22 @@
+# A python slot machine
+
+def spin_row():
+    pass
+
+def print_row():
+    pass
+
+def get_payout():
+    pass
+
+def main():
+    balance = 100
+
+    print("**************************")
+    print("Welcome to python slot")
+    print("Symbols: 🍒 🍉 🍋 🔔 ⭐ ")
+    print("**************************")
+
+
+if __name__ == "__main__":
+    main()
